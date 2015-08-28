d529a7a93c0a0f90649c6fb9b7d7c39fbad7f4e6
Alexa <amzeazas@comcast.net>
2015-08-28 08:37:57 -0700

    Add code to Band class, pass.

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -1,5 +1,7 @@
+require('spec_helper')
+
 describe(Band) do
-  it { should have_many(:venues) }
+  it { should have_and_belong_to_many(:venues) }
   it { should validate_presence_of(:name) }
 
   it('capitalizes the first letter of each word in the name') do
