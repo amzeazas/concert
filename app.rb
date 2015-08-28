295397b93b8f1c2fff99587438e5de8f986dd611
Alexa <amzeazas@comcast.net>
2015-08-28 09:06:43 -0700

    Add new bands form.

--- app.rb
+++ app.rb
@@ -6,3 +6,25 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get('/band/new') do
+  erb(:band_form)
+end
+
+post('/bands') do
+  @band = Band.create({:name => params.fetch("name")})
+  redirect('/bands')
+end
+
+get('/venue/new') do
+  #add info for adding new venue page
+end
+
+get('/bands') do
+  @bands = Band.all()
+  erb(:bands)
+end
+
+get('/venues') do
+  #add info for venues page
+end
