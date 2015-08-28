3ea172eb7b3ab2f6b2e8ddc2d4803c1f9c2420a6
Alexa <amzeazas@comcast.net>
2015-08-28 10:28:14 -0700

    Add integration spec for deleting a band, pass.

--- spec/band_pages_spec.rb
+++ spec/band_pages_spec.rb
@@ -1,5 +1,6 @@
 require('capybara/rspec')
 require('./app')
+require('spec_helper')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
@@ -10,5 +11,16 @@ describe("adding a band", {:type => :feature}) do
     fill_in("name", :with => "Bon jovi")
     click_button("Add Band")
     expect(page).to have_content("Bon Jovi")
+  end
+end
+
+describe("deleting a band", {:type => :feature}) do
+  it("allows a user to delete a band") do
+    visit("/")
+    test_band = Band.create({:name => "Blue Man Group"})
+    click_link("SEE ALL BANDS")
+    click_link("Blue Man Group")
+    click_button("DELETE BAND")
+    expect(page).to have_content("Bands")
   end
 end
