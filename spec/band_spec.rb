8d9de25dcf43798bbc769385ae37eb988e8af6cd
Alexa <amzeazas@comcast.net>
2015-08-28 08:32:20 -0700

    Add specs for Band class.

---
+++ spec/band_spec.rb
@@ -0,0 +1,9 @@
+describe(Band) do
+  it { should have_many(:venues) }
+  it { should validate_presence_of(:name) }
+
+  it('capitalizes the first letter of each word in the name') do
+    test_band = Band.create({:name => "bon jovi"})
+    expect(test_band.name()).to(eq("Bon Jovi"))
+  end
+end
